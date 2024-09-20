a9d83df79973531b6dfaadc2befb0cd6b96cc53d
TnTWoW <yuzezhao@mail.ustc.edu.cn>
2024-09-20 22:13:39 +0800

    add coc and cot implement

--- prompts/cruxeval.py
+++ prompts/cruxeval.py
@@ -1,83 +1,85 @@
 input_prompt = """You will be given a function f and an output in the form f(??) == output. Find any input such that 
-executing f on the input leads to the given output. There may be multiple answers, but you should only output one. In 
-[ANSWER] and [/ANSWER] tags, complete the assertion with one such input that will produce the output when executing 
-the function.
+executing f on the input leads to the given output. There may be multiple answers, but you should only output one. 
+Complete the assertion with one such input that will produce the output when executing the function.
 
-[PYTHON]
+```python
 def f(s1, s2):
     return s1+s2
-assert f(??) == "banana"
-[/PYTHON]
-[ANSWER]
+# assert f(??) == "banana"
+```
+Answer:
+```python
 assert f("ba", "nana") == "banana"
-[/ANSWER]
+```
 
-[PYTHON]
+```python
 {code}
-assert f(??) == {output}
-[/PYTHON]
-[ANSWER]
+# assert f(??) == {output}
+```
+Answer:
+```python
 """
 
 cot_input_prompt = """You will be given a function f and an output in the form f(??) == output. Your task is to find 
 any input such that executing f on the input leads to the given output. There may be multiple answers, 
-but only output one. First, think step by step. You MUST surround the answer with [ANSWER] and [/ANSWER] tags. 
-Express your answer as a passing assertion containing the input and the given output.
+but only output one. First, think step by step. Express your answer as a passing assertion containing the input and 
+the given output.
 
-[PYTHON]
+'''python
 def f(x):
     return x + 1
-assert f(??) == 17
-[/PYTHON]
-[THOUGHT]
+# assert f(??) == 17
+```
+Let's analyze the code step by step:
 To find an input such that executing f on the input leads to the given output, we can work backwards from the given assertion.
 We know that f(??) == 17. Since the function f(x) returns x + 1, for f(??) to be equal to 17, the value of ?? should be 16. 
-[/THOUGHT]
-[ANSWER]
-assert f(16) == 17
-[/ANSWER]
 
-[PYTHON]
+Answer:
+```python
+assert f(16) == 17
+```
+
+```python
 {code}
-assert f(??) == {output}
-[/PYTHON]
-[THOUGHT]
+# assert f(??) == {output}
+```
+Let's analyze the code step by step:
 """
 
 output_prompt = """You are given a Python function and an assertion containing an input to the function. Complete the 
 assertion with a literal (no unsimplified expressions, no function calls) containing the output when executing the 
-provided code on the given input, even if the function is incorrect or incomplete. Do NOT output any extra 
-information. Provide the full assertion with the correct output in [ANSWER] and [/ANSWER] tags, following the examples.
+provided code on the given input, even if the function is incorrect or incomplete. Provide the full assertion with 
+the correct output, following the examples.
 
-[PYTHON]
+```python
 def f(s):
     return s + "a"
-assert f("x9j") == ??
-[/PYTHON]
-[ANSWER]
+# assert f("x9j") == ??
+```
+Answer:
+```python
 assert f("x9j") == "x9ja"
-[/ANSWER]
+```
 
-[PYTHON]
+```python
 {code}
-assert f({input}) == ??
-[/PYTHON]
-[ANSWER]
+# assert f({input}) == ??
+```
+Answer:
+```python
 """
 
 cot_output_prompt = """You are given a Python function and an assertion containing an input to the function. Complete 
 the assertion with a literal (no unsimplified expressions, no function calls) containing the output when executing 
-the provided code on the given input, even if the function is incorrect or incomplete. Do NOT output any extra 
-information. Execute the program step by step before arriving at an answer, and provide the full assertion with the 
-correct output in [ANSWER] and [/ANSWER] tags, following the examples.
+the provided code on the given input, even if the function is incorrect or incomplete. Execute the program step by 
+step before arriving at an answer, and provide the full assertion with the correct output, following the examples.
 
-[PYTHON]
+```python
 def f(s):
     s = s + s
     return "b" + s + "a"
-assert f("hi") == ??
-[/PYTHON]
-[THOUGHT]
+# assert f("hi") == ??
+```
 Let's execute the code step by step:
 
 1. The function f is defined, which takes a single argument s.
@@ -85,14 +87,101 @@ Let's execute the code step by step:
 3. Inside the function, s is concatenated with itself, so s becomes "hihi".
 4. The function then returns a new string that starts with "b", followed by the value of s (which is now "hihi"), and ends with "a".
 5. The return value of the function is therefore "bhihia".
-[/THOUGHT]
-[ANSWER]
-assert f("hi") == "bhihia"
-[/ANSWER]
 
-[PYTHON]
+Answer:
+```python
+assert f("hi") == "bhihia"
+```
+
+```python
 {code}
-assert f({input}) == ??
-[/PYTHON]
-[THOUGHT]
+# assert f({input}) == ??
+```
+Let's execute the code step by step:
 """
+
+output_feedback_prompt = """The predicted output {p_output} does not match the actual execution result {output}.
+Please regenerate the result for f({input})
+ 
+```python
+{code}
+# assert f({input}) == ??
+```
+Answer:
+```python
+"""
+
+coc_output_prompt = """You are given a Python function and an assertion containing an input to the function. Complete 
+the assertion with a literal (no unsimplified expressions, no function calls) containing the output when executing 
+the provided code on the given input, even if the function is incorrect or incomplete. Execute the program step by 
+step before arriving at an answer, and provide the full assertion with the correct output, following the examples.
+
+```python
+def f(s):
+    s = s + s
+    result = "b" + s + "a"
+    return result
+# assert f("hi") == ??
+```
+[TRACE]
+state: {{}}
+line: f("hi")
+explanation: Python execution.
+delta state: {{'s': 'hi'}}
+line: s = s + s
+explanation: Python execution.
+delta state: {{'s': 'hihi'}}
+line: result = "b" + s + "a"
+explanation: Python execution.
+delta state: {{'result': 'bhihia'}}
+line: return result
+explanation: Python execution.
+delta state: {{}}
+[/TRACE]
+Answer:
+```python
+assert f("hi") == "bhihia"
+```
+
+```python
+{code}
+# assert f({input}) == ??
+```
+[TRACE]
+"""
+
+rule_output_prompt = """You are given a piece of Python function. Please analyze the functionality of the Python 
+code step by step, and based on the input {input} and the function's purpose, provide the full assertion with the 
+correct output.
+
+'''python
+{code}
+'''
+
+Please provide the analysis and the assertion in the following format:
+Your analysis: <Your analysis>
+Answer:
+```python
+assert f({input}) == <Your output>
+```
+"""
+
+rule_with_feedback_output_prompt = """You are given a piece of Python function:
+
+'''python
+{code}
+'''
+
+And the following is your analysis of the code: {rule}
+
+The predicted output {p_output} based on your analysis does not match the actual execution result {output}. Please reflect 
+on the potential errors in the analysis steps and provide the full assertion with the correct output.
+
+Please provide the analysis and the assertion in the following format:
+Your analysis: <Your analysis>
+Answer:
+```python
+assert f({input}) == <Your output>
+```
+"""
+
--- tasks/cruxeval.py
+++ tasks/cruxeval.py
@@ -1,39 +1,90 @@
 import logging
 import re
+import numpy as np
 
 from prompts.cruxeval import (
     input_prompt,
     cot_input_prompt,
     output_prompt,
     cot_output_prompt,
+    coc_output_prompt,
+    rule_output_prompt,
+    rule_with_feedback_output_prompt,
 )
-from tasks.base import PythonTask
+from tasks.base import Task
 from utils.format_utils import format_grid, format_list, str_to_list, unformat_grid
 from utils.query_utils import CLAUDE_MODELS
 
 logger = logging.getLogger(__name__)
 
-class CruxEvalInput(PythonTask):
+class CruxEvalInput(Task):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.io_prompt = input_prompt
-        self.rule_to_output_prompt = rule_to_output_prompt
 
-        if self.rule_type == "python":
-            if 'Llama' in self.model_name:
-                self.rule_prompt = llama_python_rule_prompt
-                self.rule_with_feedback_prompt = llama_rule_with_feedback_prompt
-                self.coc_prompt = coc_prompt
-            else:
-                self.rule_prompt = python_rule_prompt
-                self.rule_with_feedback_prompt = rule_with_feedback_prompt
-                self.coc_prompt = coc_prompt
-        elif self.rule_type == "noisy":
-            self.rule_prompt = noisy_rule_prompt
-            self.rule_with_feedback_prompt = nosiy_rule_with_feedback_prompt
+
+class CruxEvalOutput(Task):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.io_prompt = output_prompt
+        self.cot_prompt = cot_output_prompt
+        self.coc_prompt = coc_output_prompt
+        self.rule_prompt = rule_output_prompt
+        self.rule_with_feedback_prompt = rule_with_feedback_output_prompt
+
+    def get_code_examples(self, data):
+        return data["code"][: self.n_train]
+    def get_input_examples(self, data):
+        return data["input"][: self.n_train]
+    def get_output_examples(self, data):
+        return data["output"][: self.n_train]
+
+    def get_all_examples(self, split, idxs=None):
+        if idxs is None:
+            idxs = list(range(len(self.data)))
+        if split == "code":
+            return [self.get_code_examples(self.data[i]) for i in idxs]
+        elif split == "input":
+            return [self.get_input_examples(self.data[i]) for i in idxs]
+        elif split == "output":
+            return [self.get_output_examples(self.data[i]) for i in idxs]
         else:
-            self.rule_prompt = structured_rule_prompt
-            self.rule_with_feedback_prompt = rule_with_feedback_prompt
-        self.example_prompt = example_prompt
-        self.feedback_prompt = feedback_prompt
-        self.rule_to_python_prompt = rule_to_python_prompt
+            raise ValueError(f"Invalid split: {split}")
+
+    def extract_output_prediction(self, text):
+        pattern = r'==\s*(.*?)\s*```'
+        results = re.findall(pattern, text)
+        if not results:
+            return text
+        return results[-1]
+    def apply_all_rules(self, idxs, all_rules, all_examples):
+        pass
+    def get_metrics(self, answers: list) -> dict:
+        all_outputs = self.get_all_examples("output")
+        acc = np.mean([eval(a) == eval(o) for a, o in zip(answers, all_outputs)])
+        output_dict = {
+            "test_instance_acc": acc,
+            "test_acc": acc,
+        }
+        return output_dict
+    def eval_io(self):
+        all_codes = self.get_all_examples("code")
+        all_inputs = self.get_all_examples("input")
+
+        prompts = []
+        idxs = []
+        for i, (code, input) in enumerate(zip(all_codes, all_inputs)):
+            if self.rule_type == 'io':
+                prompt = self.io_prompt.format(code=code, input=input)
+            elif self.rule_type == 'cot':
+                prompt = self.cot_prompt.format(code=code, input=input)
+            elif self.rule_type == 'coc':
+                prompt = self.coc_prompt.format(code=code, input=input)
+            prompts.append(prompt)
+            idxs.append(i)
+        responses = self.query(prompts, idxs, histories=None)
+        responses = self.get_best_outputs(responses)
+        responses = [self.extract_output_prediction(r) for r in responses]
+        metrics = self.get_metrics(responses)
+        self.metrics.append(metrics)
+
